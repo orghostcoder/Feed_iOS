0001297e8f1d545c1da3bc73f6e45ba23a1ce8d7
orghostcoder <codeztw@gmail.com>
2018-03-19 12:24:46 +0800

    first commit

---
+++ Feed_iOS/SupportingFiles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Feed_iOS
+//
+//  Created by zhao tianwei on 2018/3/15.
+//  Copyright © 2018年 Feed.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
